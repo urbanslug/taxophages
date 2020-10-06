c9a1c70be1b0d6af99c132a8667d4cd1dca5f4d4
Njagi Mwaniki <njagi@urbanslug.com>
2020-10-06 13:06:44 +0300

    Add mapping using dozyg

---
+++ Scripts/mapping_dozyg.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# set vars
+SEQUENCE=data/downloaded/first.fasta
+INPUT_GRAPH=data/dozyg_data/relabeledSeqs.sorted_by_quality_and_len.g6.unchop.sorted.odgi
+OUTPUT_DOZYG=output/first_sequence_dozyg.gaf
+
+# Build the index
+# Very fast index barely a second.
+echo "Indexing ${INPUT_GRAPH}"
+dozyg index -i $INPUT_GRAPH -k 15 -e 3 -t 4
+
+# Map the first sequence to the graph
+echo "Mapping ${SEQUENCE} to ${INPUT_GRAPH}"
+dozyg map -i $INPUT_GRAPH -f $SEQUENCE > $OUTPUT_DOZYG
