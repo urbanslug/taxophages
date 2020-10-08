fe15237c4a8911ac21aecf7a3aedd473bc1ff631
Njagi Mwaniki <njagi@urbanslug.com>
2020-10-08 17:29:13 +0300

    Get a transposed matrix
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- Scripts/tiny_rSVD.R
+++ Scripts/tiny_rSVD.R
@@ -9,11 +9,24 @@ suppressPackageStartupMessages(
 
 setwd("~/src/Work/UT/taxophages/")
 
+# read the data into a dataframe
 data.df <- read.delim("Data/DRB1-3123_matrix.tsv")
 
-# 
-coverage.df <- data.df[,4:ncol(data.df)]
-coverage.matrix <- as.matrix(coverage.df)
+# number of samples
 sample_count <- nrow(coverage.df)
 
+# isolate only the coverage data
+coverage.df <- data.df[,4:ncol(data.df)]
+
+# convert the coverage data into a matrix
+coverage.matrix <- as.matrix(coverage.df)
+
+# run rSVD
 coverage.rsvd <- rsvd(coverage.matrix)
+
+# transpose the matrix
+# -----
+
+# run rSVD on the transposed matrix; gives us a 12x12 v matrix.
+coverage.matrix.t <- t(coverage.matrix)
+coverage.rsvd.t <- rsvd(coverage.matrix.t)
