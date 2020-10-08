dfbe9e87950fb47773179e82630ddfc481255cef
Njagi Mwaniki <njagi@urbanslug.com>
2020-10-08 17:19:32 +0300

    Remove other deps
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- Scripts/tiny_rSVD.R
+++ Scripts/tiny_rSVD.R
@@ -3,14 +3,7 @@
 # Imports ----
 suppressPackageStartupMessages(
   {
-    require(dplyr)
-    require(FactoMineR)
-    require(ggplot2)
-    require(ggtree)
     require(rsvd)
-    require(FactoMineR)
-    require(pracma)
-    require(ape)
   })
 
 
@@ -21,6 +14,6 @@ data.df <- read.delim("Data/DRB1-3123_matrix.tsv")
 # 
 coverage.df <- data.df[,4:ncol(data.df)]
 coverage.matrix <- as.matrix(coverage.df)
-sample_count = nrow(coverage.df)
+sample_count <- nrow(coverage.df)
 
-coverage.rsvd <- rsvd(coverage.matrix, k=sample_count)
+coverage.rsvd <- rsvd(coverage.matrix)
